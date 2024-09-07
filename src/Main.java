2456fd41ccb999b47a4304b8157bda8d66c09840
Leldom <Kannix1402@gmail.com>
2024-09-07 07:46:48 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < ascii().length(); i++)
+            System.out.print(ascii().charAt(i));
+    }
+    public static String ascii() {
+        return "  ／l、             \n" +
+                "（ﾟ､ ｡ ７         \n" +
+                "  l  ~ヽ       \n" +
+                "  じしf_,)ノ";
+    }
+}
